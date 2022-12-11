426ad52cd1992b5dfd56d9a45f7d92874f0a1850
Erick Marques <54284734+erickfmarks@users.noreply.github.com>
2022-12-10 22:36:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,20 @@ $(document).ready(function(){
     });
 });
 
+window.onload = function(){
+  $(".loader").fadeOut("slow");
+};
 
 function menu(btn){
     document.getElementById("home").style.display = "none";
-    document.getElementById("pubs").style.display = "none";
     document.getElementById("event").style.display = "none";
     document.getElementById("promo").style.display = "none";
     document.getElementById("patro").style.display = "none";
     document.getElementById(btn).style.display = "block";
 }
 
+$( "#sel_regiao" ).change(function() {
+  $(".loader").fadeIn();
+  
+  $(".loader").delay(800).fadeOut("slow");
+});
